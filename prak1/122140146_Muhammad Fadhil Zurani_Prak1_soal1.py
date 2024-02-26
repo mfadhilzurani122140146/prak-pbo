70078bd6bbee2e91088e3f9e5017f3a76f719f2f
mfadhilzurani122140146 <161264541+mfadhilzurani122140146@users.noreply.github.com>
2024-02-26 08:31:11 +0700

    Update 122140146_Muhammad Fadhil Zurani_Prak1_soal1.py

--- prak1/122140146_Muhammad Fadhil Zurani_Prak1_soal1.py
+++ prak1/122140146_Muhammad Fadhil Zurani_Prak1_soal1.py
@@ -1,1 +1,17 @@
+def hitung_bilangan_ganjil(batas_bawah, batas_atas):
+    if batas_bawah < 0 or batas_atas < 0:
+        print("Batas bawah dan atas yang dimasukan tidak boleh di bawah Nol")
+        return
 
+    jumlah_ganjil = 0
+    for i in range(batas_bawah, batas_atas + 1):
+        if i % 2 != 0:
+            print(i)
+            jumlah_ganjil += i
+
+    print("Total:", jumlah_ganjil)
+
+batas_bawah = int(input("batas bawah: "))
+batas_atas = int(input("batas atas: "))
+
+hitung_bilangan_ganjil(batas_bawah, batas_atas)
