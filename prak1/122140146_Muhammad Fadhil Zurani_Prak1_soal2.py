e32999a3eefc69b8aabdac440975c98fb22dcc0a
mfadhilzurani122140146 <161264541+mfadhilzurani122140146@users.noreply.github.com>
2024-02-26 08:52:04 +0700

    Update 122140146_Muhammad Fadhil Zurani_Prak1_soal2.py

--- prak1/122140146_Muhammad Fadhil Zurani_Prak1_soal2.py
+++ prak1/122140146_Muhammad Fadhil Zurani_Prak1_soal2.py
@@ -1,1 +1,18 @@
+import math
 
+def hitung_luas(jari_jari):
+    return math.pi * jari_jari ** 2
+
+def hitung_keliling(jari_jari):
+    return 2 * math.pi * jari_jari
+
+jari_jari = float(input("jari-jari: "))
+
+if jari_jari < 0:
+    print("jari-jari lingkaran tidak boleh negatif.")
+else:
+    luas = hitung_luas(jari_jari)
+    keliling = hitung_keliling(jari_jari)
+
+    print("Luas:", round(luas, 2))
+    print("Keliling:", round(keliling, 2))
