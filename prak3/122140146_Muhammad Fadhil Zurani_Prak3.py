be89900bd807135a2de364432c09dfe0da7c0cc7
mfadhilzurani122140146 <161264541+mfadhilzurani122140146@users.noreply.github.com>
2024-03-11 01:12:14 +0700

    Update 122140146_Muhammad Fadhil Zurani_Prak3.py

--- prak3/122140146_Muhammad Fadhil Zurani_Prak3.py
+++ prak3/122140146_Muhammad Fadhil Zurani_Prak3.py
@@ -1,1 +1,33 @@
+class Dagangan:
+    jumlah_barang = 0
+    list_barang = []
 
+    def __init__(self, nama, stok, harga):
+        self.__nama = nama
+        self.__stok = stok
+        self.__harga = harga
+        Dagangan.jumlah_barang += 1
+        Dagangan.list_barang.append((nama, stok, harga))
+
+    def __del__(self):
+        Dagangan.jumlah_barang -= 1
+        for i, barang in enumerate(Dagangan.list_barang):
+            if barang[0] == self.__nama:
+                del Dagangan.list_barang[i]
+                print(f"{self.__nama} dihapus dari toko!")
+                break
+
+    @classmethod
+    def lihat_barang(cls):
+        print(f"Jumlah barang dagangan pada toko: {cls.jumlah_barang} buah")
+        for i, barang in enumerate(cls.list_barang):
+            print(f"{i+1}. {barang[0]} seharga Rp {barang[2]} (stok: {barang[1]})")
+
+
+Dagangan1 = Dagangan("Galon Aqua 19L", 32, 17000)
+Dagangan2 = Dagangan("Gas LPG 5 kg", 22, 88000)
+Dagangan3 = Dagangan("Beras Ramos 5 kg", 13, 68000)
+Dagangan.lihat_barang()
+
+del Dagangan1
+Dagangan.lihat_barang()
