66aaee2a19d5203e89cbbac0a73553b33b1be2a8
mfadhilzurani122140146 <161264541+mfadhilzurani122140146@users.noreply.github.com>
2024-03-28 20:35:31 +0700

    Update 122140146_Muhammad Fadhil Zurani_Prak4_soal2.py

--- prak4/122140146_Muhammad Fadhil Zurani_Prak4_soal2.py
+++ prak4/122140146_Muhammad Fadhil Zurani_Prak4_soal2.py
@@ -1,1 +1,26 @@
+import math
+
+class Bentuk:
+    def hitungLuas(self):
+        pass
+
+class Persegi(Bentuk):
+    def __init__(self, sisi):
+        self.sisi = sisi
+    
+    def hitungLuas(self):
+        return self.sisi ** 2
+
+class Lingkaran(Bentuk):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def hitungLuas(self):
+        return math.pi * self.radius ** 2
+
+persegi = Persegi(5)
+lingkaran = Lingkaran(3)
+
+print(f"Luas Persegi: {persegi.hitungLuas()}") 
+print(f"Luas Lingkaran: {lingkaran.hitungLuas()}") 
 
